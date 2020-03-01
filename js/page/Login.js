26f60767257139651e31264d237cfa3b4dcb383b
huanglu <790478146@qq.com>
2020-03-01 10:57:15 +0800

    旋转动画

--- js/page/Login.js
+++ js/page/Login.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Animated, Easing, Image } from 'react-native';
 import actions from '../action';
 import { connect } from 'react-redux';
 
 class Login extends Component {
+    constructor() {
+        super()
+        this.spinValue = new Animated.Value(0)
+    }
+
+    componentDidMount() {
+        this.spin()
+    }
+    spin() {
+        this.spinValue.setValue(0)
+        Animated.timing(
+            this.spinValue,
+            {
+                toValue: 1,
+                duration: 4000,
+                easing: Easing.linear
+            }
+        ).start(() => this.spin())
+    }
+
     gotoWorkerPage() {
         const { navigation } = this.props;
         console.log(this.props)
@@ -15,11 +35,24 @@ class Login extends Component {
         console.log(this.props)
         navigation.navigate('WorkshopDirector');
     }
+
     render() {
         const { navigation } = this.props;
-        console.log("navigation",navigation)
+        console.log("navigation", navigation);
+        const spin = this.spinValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+        })
         return (
             <View style={styles.container}>
+                <Animated.Image
+                    style={{
+                        width: 227,
+                        height: 200,
+                        transform: [{ rotate: spin }]
+                    }}
+                    source={{ uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png' }}
+                />
                 <Text style={styles.welcome}>LoginPage</Text>
                 <Button
                     style={styles.contain}
@@ -34,8 +67,8 @@ class Login extends Component {
             </View>
         );
     }
-
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -47,19 +80,20 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
-        marginBottom:20
+        marginBottom: 20
     },
 
     contain: {
         padding: 20,
-        backgroundColor:'red',
-        marginBottom:20
+        backgroundColor: 'red',
+        marginBottom: 20
     },
-    button2:{
-        marginTop:20
+    button2: {
+        marginTop: 20
     }
 });
 const mapDispatchToProps = dispatch => ({
     onThemeChange: theme => dispatch(actions.onThemeChange(theme)),
 });
+
 export default connect(null, mapDispatchToProps)(Login);
