e921d45bbc875b6bb31f712158102b8eb82ca7dc
huanglu <790478146@qq.com>
2020-03-01 11:04:30 +0800

    复杂动画-marginLeft-opacity-fontSize-rotateX

--- js/page/Login.js
+++ js/page/Login.js
@@ -6,28 +6,28 @@ import { connect } from 'react-redux';
 class Login extends Component {
     constructor() {
         super()
-        this.spinValue = new Animated.Value(0)
-    }
-
-    componentDidMount() {
-        this.spin()
-    }
-    spin() {
-        this.spinValue.setValue(0)
-        Animated.timing(
-            this.spinValue,
-            {
-                toValue: 1,
-                duration: 4000,
-                easing: Easing.linear
-            }
-        ).start(() => this.spin())
+        this.animatedValue = new Animated.Value(0)
     }
 
     gotoWorkerPage() {
         const { navigation } = this.props;
         console.log(this.props)
         navigation.navigate('Worker');
+    }
+
+    componentDidMount() {
+        this.animate()
+    }
+    animate() {
+        this.animatedValue.setValue(0)
+        Animated.timing(
+            this.animatedValue,
+            {
+                toValue: 1,
+                duration: 2000,
+                easing: Easing.linear
+            }
+        ).start(() => this.animate())
     }
 
     gotoWorkshopDirector() {
@@ -39,20 +39,69 @@ class Login extends Component {
     render() {
         const { navigation } = this.props;
         console.log("navigation", navigation);
-        const spin = this.spinValue.interpolate({
+        const marginLeft = this.animatedValue.interpolate({
             inputRange: [0, 1],
-            outputRange: ['0deg', '360deg']
+            outputRange: [0, 300]
+        })
+        const opacity = this.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [0, 1, 0]
+        })
+        const movingMargin = this.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [0, 300, 0]
+        })
+        const textSize = this.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [18, 32, 18]
+        })
+        const rotateX = this.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ['0deg', '180deg', '0deg']
         })
         return (
             <View style={styles.container}>
-                <Animated.Image
+                <Animated.View
                     style={{
-                        width: 227,
-                        height: 200,
-                        transform: [{ rotate: spin }]
-                    }}
-                    source={{ uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png' }}
-                />
+                        marginLeft,
+                        height: 30,
+                        width: 40,
+                        backgroundColor: 'red'
+                    }} />
+                <Animated.View
+                    style={{
+                        opacity,
+                        marginTop: 10,
+                        height: 30,
+                        width: 40,
+                        backgroundColor: 'blue'
+                    }} />
+                <Animated.View
+                    style={{
+                        marginLeft: movingMargin,
+                        marginTop: 10,
+                        height: 30,
+                        width: 40,
+                        backgroundColor: 'orange'
+                    }} />
+                <Animated.Text
+                    style={{
+                        fontSize: textSize,
+                        marginTop: 10,
+                        color: 'green'
+                    }} >
+                    Animated Text!
+                </Animated.Text>
+                <Animated.View
+                    style={{
+                        transform: [{ rotateX }],
+                        marginTop: 50,
+                        height: 30,
+                        width: 40,
+                        backgroundColor: 'black'
+                    }}>
+                    <Text style={{ color: 'white' }}>Hello from TransformX</Text>
+                </Animated.View>
                 <Text style={styles.welcome}>LoginPage</Text>
                 <Button
                     style={styles.contain}
